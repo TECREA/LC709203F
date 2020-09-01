46531c892016b565853b1f2553108c7614d3f232
Manuel Posada Calderon <64924462+Manuelpc10@users.noreply.github.com>
2020-09-01 10:35:42 -0500

    Update LC709203F.h
    Minor update .h

--- LC709203F.h
+++ LC709203F.h
@@ -10,38 +10,17 @@
 
   #include <stdint.h>
 
-   
-  /* EXAMPLE FOR I2C READ/WRITE wrappers
-   **
-   **   void Write_I2C(uint8_t Address, void *data, uint8_t amount){
-   **   	uint8_t *DatatoSend = (uint8_t *)data;
-   **   	HAL_I2C_Master_Transmit(&hi2c1,Address,DatatoSend,amount,10);
-   **   }
-   **
-   **   void Read_I2C(uint8_t Address, void *Register, uint8_t amount){
-   **   	uint8_t *DatatoSend = (uint8_t *)Register;
-   **   	HAL_I2C_Master_Transmit(&hi2c1,Address,DatatoSend, 1, 10);
-   **   	HAL_I2C_Master_Receive(&hi2c1,Address,DatatoSend, amount, 10);
-   **   }
-  */   
+  #define LC709203F_ADDR_SLAVE                   ( 0x0Bu )            /* address slave */
+  #define LC709203F_VCELL                        ( 0x09u )            /* cell voltage */
+  #define LC709203F_RSOC                         ( 0x0Du )            /* Relative state of charge */
+  #define LC709203F_REG_ITE                      ( 0x0Fu )            /* Indicator to empty */
+  #define LC709203F_REG_ID                       ( 0x11u )            /* chip id */
 
-
-  #define LC709203F_ADDR_SLAVE                   ( 0x0Bu )             /*address slave*/
-  #define LC709203F_VCELL                        ( 0x09u )            /*cell voltage*/
-  #define LC709203F_RSOC                         ( 0x0Du )            /*Relative state of charge*/
-  #define LC709203F_REG_ITE                      ( 0x0Fu )         /*Indicator to empty*/
-
-
-
-  /* CONFIG Register Format*/
-  #define LC709203F_CONFIG_SLEEP_BIT		        ( 07u )
-
-  /*Resolution for calculate Vcell*/
-  #define LC709203F_VCELL_RESOLUTION	            ( 78125 )
-
-
-  typedef void (*Write_Fcn )(uint8_t, void*, uint8_t);    // Pointer to functions type
-  typedef void (*Read_Fcn  )(uint8_t, void*, uint8_t);    //
+/**
+ * Pointer to functions type
+ */
+  typedef void (*Write_Fcn )(uint8_t, void*, uint8_t);
+  typedef void (*Read_Fcn  )(uint8_t, void*, uint8_t);
 
   typedef struct {
     Write_Fcn Write;
@@ -49,18 +28,67 @@
     uint8_t   Address;
   }LC709203F_t;
 
-  /*============================================================================*/
+  /**
+   * @brief Initialize LC709203F module to work
+   * @param Obj Obj Structure containing all data from the LC709203F module.
+   * @param Write pointer to function i2c write
+   * @param Read  pointer to function i2c read
+   * @param Addres_Device slave addres device
+   */
   void LC709203F_Init(LC709203F_t *Obj, Write_Fcn Write, Read_Fcn Read, uint8_t Addres_Device);
-  /*============================================================================*/
+
+  /**
+   * @brief this function read register
+   * @param Obj     Obj Structure containing all data from the LC709203F module.
+   * @param Reg     address register to write
+   * @param data    data to write
+   * @param amount  length bytes to write
+   */
   void LC709203F_Write(LC709203F_t *Obj, uint8_t Reg, uint16_t data, uint8_t amount);
-  /*============================================================================*/
+
+  /**
+   * @brief this function read register
+   * @param Obj     Obj Structure containing all data from the LC709203F module.
+   * @param Reg     address register to read
+   * @param amount  length bytes to read
+   * @return        data received in 2 bytes
+   */
   uint16_t LC709203F_Read(LC709203F_t *Obj, uint8_t Reg, uint8_t amount);
-  /*============================================================================*/
+
+  /**
+   * @param this function read voltage from battery
+   * @param Obj Obj Structure containing all data from the LC709203F module.
+   * @return Voltage read on battery in mV
+   */
   uint16_t LC709203F_Voltage(LC709203F_t *Obj);
-  /*============================================================================*/
-   uint16_t LC709203F_SOC(LC709203F_t *Obj);
-  /*============================================================================*/
+
+  /**
+   * @brief this function read soc in float
+   * @param Obj Obj Structure containing all data from the LC709203F module.
+   * @return state of charge float
+   */
+  uint16_t LC709203F_SOC(LC709203F_t *Obj);
+
+  /**
+   * @brief This function request ID
+   * @param Obj Obj Structure containing all data from the LC709203F module.
+   * @return version module
+   */
+  uint16_t LC709203F_ID (LC709203F_t *Obj);
 
 
+   /* EXAMPLE FOR I2C READ/WRITE wrappers
+    **
+    **   void Write_I2C(uint8_t Address, void *data, uint8_t amount){
+    **   	uint8_t *DatatoSend = (uint8_t *)data;
+    **   	HAL_I2C_Master_Transmit(&hi2c1,Address,DatatoSend,amount,10);
+    **   }
+    **
+    **   void Read_I2C(uint8_t Address, void *Register, uint8_t amount){
+    **   	uint8_t *DatatoSend = (uint8_t *)Register;
+    **   	HAL_I2C_Master_Transmit(&hi2c1,Address,DatatoSend, 1, 10);
+    **   	HAL_I2C_Master_Receive(&hi2c1,Address,DatatoSend, amount, 10);
+    **   }
+   */
 
 #endif /* DRIVERS_HD_LC709203F_H_ */
