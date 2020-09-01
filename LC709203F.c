67ca0a0e796035d9fcab97beba5508c3c97e7154
Manuel Posada Calderon <64924462+Manuelpc10@users.noreply.github.com>
2020-09-01 10:34:13 -0500

    Update LC709203F.c
    minor Update .c file

--- LC709203F.c
+++ LC709203F.c
@@ -7,8 +7,8 @@ void LC709203F_Init(LC709203F_t *Obj, Write_Fcn Write, Read_Fcn Read, uint8_t Ad
   Obj->Write = Write;
   Obj->Read = Read;
   Obj->Address = Addres_Device;
- // if(SleepEnable) LC709203F_SleepEnable(Obj);
 }
+
 /*
  ** ==================== Write on any general two bytes register  ====================
  */
@@ -21,7 +21,6 @@ void LC709203F_Write(LC709203F_t *Obj, uint8_t Reg, uint16_t data, uint8_t amoun
   Obj->Write(Obj->Address, (void*)buffer, (amount+1));
 }
 
-
 /*
  ** ==================== read two bytes register ====================
  */
@@ -29,20 +28,26 @@ uint16_t LC709203F_Read(LC709203F_t *Obj, uint8_t Reg, uint8_t amount){
   uint8_t buffer[3];
   buffer[0] = Reg;
   Obj->Read(Obj->Address, buffer, amount);
-  //return buffer[0] << 8 | buffer[1];
   return buffer[1] << 8 | buffer[0];
-  //return buffer;
 }
+
 /*
  ** ==================== Get battery voltage ====================
  */
 uint16_t LC709203F_Voltage(LC709203F_t *Obj){
 	return (uint16_t) ((LC709203F_Read(Obj, LC709203F_VCELL, 2)));
 }
+
 /*
  ** ==================== Get battery SOC (State of charge) ====================
  */
 uint16_t LC709203F_SOC(LC709203F_t *Obj){
 	return (uint16_t) ((LC709203F_Read(Obj, LC709203F_RSOC, 2)));
 }
+
+/** ==================== Get ID chip SOC  ===================================*/
+uint16_t LC709203F_ID(LC709203F_t *Obj){
+	return (uint16_t) ((LC709203F_Read(Obj, LC709203F_REG_ID, 2)));
+}
+
 
